d531b029ecdc1392f8f267ce4c4226fc16615563
Rafał Szczuka <rafal.szczuka05@gmail.com>
2020-04-03 14:23:53 +0200

    scroll to section refactor

--- js/menu.js
+++ js/menu.js
@@ -5,33 +5,14 @@ const menuBar = document.querySelector(".menu-bar");
 const menuItems = document.querySelectorAll(".menu-item");
 const menuLogo = document.querySelector(".menu-logo img");
 
-const menuAbout = document.getElementById("about");
-const menuNews = document.getElementById("news");
-const menuGallery = document.getElementById("gallery");
-const menuOrder = document.getElementById("order");
-const menuContact = document.getElementById("contact");
-
 const burgerBtn = document.querySelector(".burger");
 const burgerOn = document.querySelector(".fas.fa-bars");
 const burgerOff = document.querySelector(".fas.fa-times");
 
 const socialsIcons = document.querySelectorAll(".socials i");
 
-// sections
-const sectionHome = document.querySelector(".home");
-const sectionAbout = document.querySelector(".about");
-const sectionNews = document.querySelector(".news");
-const sectionGallery = document.querySelector(".gallery");
-const sectionOrder = document.querySelector(".order");
-const sectionContact = document.querySelector(".contact");
-
-// sections from top
-const sectionHomeFromTop = sectionHome.offsetTop;
-const sectionAboutFromTop = sectionAbout.offsetTop;
-const sectionNewsFromTop = sectionNews.offsetTop;
-const sectionGalleryFromTop = sectionGallery.offsetTop;
-const sectionOrderFromTop = sectionOrder.offsetTop;
-const sectionContactFromTop = sectionContact.offsetTop;
+// gallery section from top
+const sectionGalleryFromTop = document.querySelector(".gallery").offsetTop;
 
 // burger menu icon toggler
 function burgerToggler() {
@@ -67,29 +48,18 @@ function removeClassForElements(elementsArray, setClass) {
 
 // ***************** IMPLEMENTATION *****************
 
-// scroll to section
+//  listener on every menu item, scroll to coresponding by id section class name
+menuItems.forEach(item => {
+  item.addEventListener("click", function() {
+    const offset = document.querySelector(`.${this.id}`).offsetTop;
+    window.scrollTo(0, offset);
+    menuToggler();
+  });
+});
+
+// listener on menu logo, scroll to 0 top position
 menuLogo.addEventListener("click", function() {
-  window.scrollTo(0, sectionHomeFromTop);
-  menuToggler();
-});
-menuAbout.addEventListener("click", function() {
-  window.scrollTo(0, sectionAboutFromTop);
-  menuToggler();
-});
-menuNews.addEventListener("click", function() {
-  window.scrollTo(0, sectionNewsFromTop);
-  menuToggler();
-});
-menuGallery.addEventListener("click", function() {
-  window.scrollTo(0, sectionGalleryFromTop);
-  menuToggler();
-});
-menuOrder.addEventListener("click", function() {
-  window.scrollTo(0, sectionOrderFromTop);
-  menuToggler();
-});
-menuContact.addEventListener("click", function() {
-  window.scrollTo(0, sectionContactFromTop);
+  window.scrollTo(0, 0);
   menuToggler();
 });
 
